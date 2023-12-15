f2e5345c7c5be4d55b42be54060183fbe006e3c1
safaakan <safaaka939@gmail.com>
2023-12-15 14:47:43 -0800

    tasks0

---
+++ pop.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * _pop - prints the top
+ * @head: stack head
+ * @c: line_number
+ * Return: no return
+*/
+void _pop(stack_t **head, unsigned int c)
+{
+	stack_t *temp;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", c);
+		fclose(var.ptr_file);
+		free(var.line_cont);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	temp = *head;
+	*head = temp->next;
+	free(temp);
+}
