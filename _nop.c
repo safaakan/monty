a1df730d70b9be7851d7178a0013a9ea046be22b
safaakan <safaaka939@gmail.com>
2023-12-15 15:40:18 -0800

    tasks5

---
+++ _nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * _nop - doesn’t do anything.
+ * @head: stack head.
+ * @c: Line number.
+ * Return: No return.
+ */
+void _nop(stack_t **head, unsigned int c)
+{
+(void)head;
+(void)c;
+}
