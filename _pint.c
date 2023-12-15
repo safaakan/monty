5fa62551ea7d6f164f163a93bb5ea89ea4e4e37f
safaakan <safaaka939@gmail.com>
2023-12-15 14:28:14 -0800

    tasks0

--- _pint.c
+++ _pint.c
@@ -7,13 +7,13 @@
 */
 void _pint(stack_t **head, unsigned int c)
 {
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", c);
-		fclose(var.ptr_file);
-		free(var.line_cont);
-		free_stk(*head);
-		exit(EXIT_FAILURE);
-	}
-	printf("%d\n", (*head)->n);
+        if (*head == NULL)
+        {
+                fprintf(stderr, "L%u: can't pint, stack empty\n", c);
+                fclose(var.ptr_file);
+                free(var.line_cont);
+                free_stk(*head);
+                exit(EXIT_FAILURE);
+        }
+        printf("%d\n", (*head)->n);
 }
