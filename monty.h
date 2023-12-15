3be8aaf0e62510cbe2c206221d9d48bf33105182
hichamb2 <beihaqihicham@gmail.com>
2023-12-16 00:08:37 +0100

    add

--- monty.h
+++ monty.h
@@ -57,7 +57,7 @@ ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 void _push(stack_t **head, unsigned int c);
 void _pall(stack_t **head, unsigned int n);
 void _pint(stack_t **head, unsigned int n);
-
+void _pop(stack_t **head, unsigned int c);
 void file_print(stack_t **stack, unsigned int line_num);
 void free_stk(stack_t *head);
 int exec(char *_cont, stack_t **stack, unsigned int c, FILE *file);
