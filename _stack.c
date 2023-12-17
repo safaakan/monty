e181b75f1814fb55d29bf9f0f34df2f43ed676b5
safaakan <safaaka939@gmail.com>
2023-12-17 15:16:20 -0800

    task15

---
+++ _stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+/**
+ * _stack - free stack
+ * @head: stack head
+ * @c: line_number
+ * Return: no return
+ */
+
+void _stack(stack_t **head, unsigned int c)
+{
+	(void)head;
+	(void)c;
+
+	var.stk_que = 0;
+}
--- monty.h
+++ monty.h
@@ -70,7 +70,7 @@ void _pchar(stack_t **head, unsigned int c);
 void _pstr(stack_t **head, unsigned int c);
 void _rotl(stack_t **head,  __attribute__((unused)) unsigned int c);
 void _rotr(stack_t **head, unsigned int c);
-
+void _stack(stack_t **head, unsigned int c);
 
 void file_print(stack_t **stack, unsigned int line_num);
 void free_stk(stack_t *head);
